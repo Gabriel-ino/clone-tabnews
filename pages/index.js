03503e80736315b856c711e01f8c7bd1be5ebbd1
Gabriel Chaves <60673704+Gabriel-ino@users.noreply.github.com>
2025-10-11 15:37:47 +0000

    Agradecendo ao meu bem

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home(){
-    return <h1>Página inicial do projeto</h1>
+    return <h1>Amanda, você é a pessoa que mais me apoia em todos os momentos, Um dia vou olhar pra
+        trás e, as coisas dando certo ou não, vou me sentir feliz porque você sempre esteve comigo. Obrigado por ser
+        minha certeza e meu porto seguro quando a maior parte desse mundo só me apresenta dúvidas e eu sinto medo de seguir
+        por determinado caminho. As coisas ficam muito mais fáceis tendo você ao meu lado.
+    </h1>
 }
 
 export default Home;
