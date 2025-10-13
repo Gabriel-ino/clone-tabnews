e6474242895578625b10a5fd5081b2935cdce4d0
Gabriel Chaves <60673704+Gabriel-ino@users.noreply.github.com>
2025-10-13 15:13:20 +0000

    Adicionando scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,18 @@
-function Home(){
-    return <h1>Amanda, você é a pessoa que mais me apoia em todos os momentos, Um dia vou olhar pra
-        trás e, as coisas dando certo ou não, vou me sentir feliz porque você sempre esteve comigo. Obrigado por ser
-        minha certeza e meu porto seguro quando a maior parte desse mundo só me apresenta dúvidas e eu sinto medo de seguir
-        por determinado caminho. As coisas ficam muito mais fáceis tendo você ao meu lado.
+function Home() {
+  return (
+    <h1>
+      Amanda, você é a pessoa que mais me apoia em todos os momentos, Um dia vou
+      olhar pra trás e, as coisas dando certo ou não, vou me sentir feliz porque
+      você sempre esteve comigo. Obrigado por ser minha certeza e meu porto
+      seguro quando a maior parte desse mundo só me apresenta dúvidas e eu sinto
+      medo de seguir por determinado caminho. As coisas ficam muito mais fáceis
+      tendo você ao meu lado.
     </h1>
+  );
+}
+
+function teste() {
+  console.log("indentação errada");
 }
 
 export default Home;
