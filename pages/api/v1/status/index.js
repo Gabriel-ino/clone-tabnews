3cd4f32102850a4a9e18b9dea72358fdac4ce880
Gabriel Chaves <60673704+Gabriel-ino@users.noreply.github.com>
2025-11-01 20:21:36 +0000

    Error handling on `database.js`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,29 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1+1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "são acima da média" });
+  const updatedAt = new Date().toISOString();
+  const verifyConnection = await database.query(
+    "SELECT current_setting('server_version')  AS version;",
+  );
+  const postgresMaxConnections = await database.query("SHOW max_connections;");
+  const databaseName = process.env.POSTGRES_DB;
+  const postgresUsedConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const postgresUsedConnectionsValue = postgresUsedConnections.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        postgres_version: verifyConnection.rows[0].version,
+        max_connections: parseInt(
+          postgresMaxConnections.rows[0].max_connections,
+        ),
+        used_connections: postgresUsedConnectionsValue,
+      },
+    },
+  });
 }
 export default status;
