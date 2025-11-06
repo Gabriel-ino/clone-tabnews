81054ba06dbb1a68a7ee2dc4fd844a330deeb4b7
Gabriel Chaves <60673704+Gabriel-ino@users.noreply.github.com>
2025-11-06 17:42:43 +0000

    Adding delete test

---
+++ tests/integration/api/v1/migrations/delete.test.js
@@ -0,0 +1,9 @@
+test("DELETE method on api/v1/migrations should return status code 405 and error", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "DELETE",
+  });
+
+  const responseBody = await response.json();
+  expect(response.status).toBe(405);
+  expect(responseBody).toEqual({ error: "Method DELETE not allowed" });
+});
